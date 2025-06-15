8bb420fdf79f6771fd9bb09919a0de3b7c769884
ramanji4 <ramanji181818@gmail.com>
2025-06-15 11:06:05 +0530

    Expense-Docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,6 +7,6 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl start docker
-sudo ystemctl enable docker
+sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
